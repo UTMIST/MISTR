c5f60f8f130ab98c3403e92d3aeb7e6ce61acf44
leglesslamb <wu.rupert@outlook.com>
2020-04-13 20:14:01 -0400

    No longer deleting old pipelines when rerunning.

--- gitlab/gitlab.go
+++ gitlab/gitlab.go
@@ -62,9 +62,6 @@ func PagesUpdate() string {
 		log.Fatal(err)
 	}
 
-	// Delete the old pipeline.
-	git.Pipelines.DeletePipeline(projectID, pipeline.ID)
-
 	// Create and return successful reply.
 	reply := fmt.Sprintf("Successfully rerun pipeline: %d as %d\nSee %s.\n",
 		pipeline.ID,
