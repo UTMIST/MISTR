c6e7f2c62da2138a2ba98ec5dad76bbd90886b75
Annie <qijia.gao@mail.utoronto.ca>
2020-11-04 17:48:11 -0500

    pipelines.sh now POSIX-compliant

--- pipelines.sh
+++ pipelines.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-source ./.env
+. ./.env
 
 curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/pipelines?page=$1&per_page=$PER_PAGE&sort=asc" | jq '.[].id' > jobs.txt
